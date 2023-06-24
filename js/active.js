b423c6285f701d8b66d5062644fff04d87e49df4
ronskyde <ronaldambundo@gmail.com>
2023-06-24 15:22:20 +0300

    new	modified:   about.html new	modified:   chef.html changes	modified:   contact.html changes	new file:   img/bg-img/DSC_1549.jpg new	new file:   img/bg-img/DSC_1593.jpg new	new file:   img/bg-img/DSC_1600.jpg new	new file:   img/bg-img/DSC_1603.jpg new#	new file:   img/bg-img/DSC_1917.jpg new	new file:   img/bg-img/DSC_1981.jpg new	new file:   img/bg-img/DSC_1982.jpg new	new file:   img/bg-img/DSC_2030.jpg new	new file:   img/bg-img/DSC_2047.jpg new	new file:   img/bg-img/DSC_9432.jpg new	new file:   img/bg-img/DSC_9433.jpg new	new file:   img/bg-img/DSC_9479.jpg new	new file:   img/bg-img/akyurt.jpg new	new file:   img/bg-img/buffet.jpg new	new file:   img/bg-img/business.jpg new	new file:   img/bg-img/celebrate.jpg new	new file:   img/bg-img/cs5.jpg new	new file:   img/bg-img/cs7.jpg new	new file:   img/bg-img/dsc12.jpg new	new file:   img/bg-img/herro-42.jpg new	new file:   img/bg-img/herro-45.jpg new	new file:   img/bg-img/herro-46.jpg new	new file:   img/bg-img/lounge22.jpg new	new file:   img/bg-img/product.jpg new	new file:   img/bg-img/skydaima.jpg new	new file:   img/bg-img/thomas-bennie-1jlJrr4XGkU-unsplash.jpg new	new file:   img/bg-img/views.jpg new	new file:   img/core-img/IMG-20230622-WA0001.jpg new	new file:   img/core-img/IMG-20230622-WA0002.jpg new	new file:   img/core-img/Mups_plazaA-01.jpeg new	new file:   img/core-img/Mups_plazaA-01.jpg new	new file:   img/core-img/Mups_plazaA.jpg new	new file:   img/core-img/Mups_plazaC.jpg new	new file:   img/core-img/google.jpg new	deleted:    img/core-img/preload-1.png new	deleted:    img/core-img/preload-2.png new	deleted:    img/core-img/preload-3.png new	deleted:    img/core-img/wings insta.docx new	modified:   index.html new	modified:   js/active.js new	modified:   rooftop.html new	modified:   style.css new	modified:   sunset.html

--- js/active.js
+++ js/active.js
@@ -143,4 +143,81 @@
         });
     });
 
+    /**
+* Card Carousel Container
+*/
+const CardCarouselContainer = document.querySelectorAll('.card-carousel-container')
+ 
+CardCarouselContainer.forEach(container =>{
+    //Active Card Item
+    var activeItem = container.querySelector('.card-carousel-item.active')
+ 
+    //Previous Sibling of Active Item
+    var activePrevSibling = activeItem.previousElementSibling
+    //Carousel Navigation
+    var nextNav = container.querySelector('.card-nav-next')
+    var prevNav = container.querySelector('.card-nav-prev')
+ 
+    //Adding the Active Item's Previous sibling with class
+    if(activePrevSibling !== null)
+        activePrevSibling.classList.add('left');
+ 
+    // Trigger next Card Item
+    nextNav.addEventListener('click', function(e){
+        e.preventDefault()
+        navigateNextCard()
+    })
+    // Trigger previuous Card Item
+    prevNav.addEventListener('click', function(e){
+        e.preventDefault()
+        navigatePrevCard()
+    })
+ 
+    // Next Card Function
+    const navigateNextCard = function(){
+        if(activeItem.nextElementSibling == null ||(activeItem.nextElementSibling != null && !activeItem.nextElementSibling.classList.contains('card-carousel-item')))
+            return false;
+        if(activePrevSibling !== null && activePrevSibling.classList.contains('left'))
+            activePrevSibling.classList.remove('left');
+        activeItem.nextElementSibling.classList.add('active')
+        activeItem.classList.remove('active')
+        activeItem = activeItem.nextElementSibling
+        activePrevSibling = activeItem.previousElementSibling
+        activePrevSibling.classList.add('left')
+        if(activeItem.nextElementSibling == null ||(activeItem.nextElementSibling != null && !activeItem.nextElementSibling.classList.contains('card-carousel-item')))
+            nextNav.style.display = 'none';
+        else
+            nextNav.style.display = 'flex';
+        if(activeItem.previousElementSibling == null ||(activeItem.previousElementSibling != null && !activeItem.previousElementSibling.classList.contains('card-carousel-item')))
+            prevNav.style.display = 'none';
+        else
+            prevNav.style.display = 'flex';
+ 
+ 
+    }
+    // Previous Card Function
+    const navigatePrevCard = function(){
+        console.log(activeItem.previousElementSibling)
+        if(activeItem.previousElementSibling == null ||(activeItem.previousElementSibling != null && !activeItem.previousElementSibling.classList.contains('card-carousel-item')))
+            return false;
+        if(activePrevSibling !== null && activePrevSibling.classList.contains('left'))
+            activePrevSibling.classList.remove('left');
+        activeItem.previousElementSibling.classList.add('active')
+        activeItem.classList.remove('active')
+        activeItem = activeItem.previousElementSibling
+        activePrevSibling = activeItem.previousElementSibling
+        if(activePrevSibling !== null && !activePrevSibling.classList.contains('left'))
+            activePrevSibling.classList.add('left');
+        if(activeItem.nextElementSibling == null ||(activeItem.nextElementSibling != null && !activeItem.nextElementSibling.classList.contains('card-carousel-item')))
+            nextNav.style.display = 'none';
+        else
+            nextNav.style.display = 'flex';
+        console.log(activeItem.previousElementSibling)
+        if(activeItem.previousElementSibling == null ||(activeItem.previousElementSibling != null && !activeItem.previousElementSibling.classList.contains('card-carousel-item')))
+            prevNav.style.display = 'none';
+        else
+            prevNav.style.display = 'flex';
+    }
+})
+
 })(jQuery);
